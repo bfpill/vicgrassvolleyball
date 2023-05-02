c4a9d5b3ce45705a7ca5e63bfef3240484086a3d
bfpill <max@v3rv.com>
2023-05-02 14:21:25 +1000

    Image Changes

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -1,7 +1,6 @@
 import React from "react";
-import teamShot1 from "../images/teamShot1.JPG";
-import teamShot2 from "../images/teamShot2.JPG";
-import { InstagramEmbed } from 'react-social-media-embed';
+import teamShot1 from "../images/teamShot1.png";
+import teamShot2 from "../images/teamShot2.png";
 import "./InfoSection.css";
 
 const formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSceX01rfyydjtwKSMEcpp_vKb7Mchkm_Am5LJ22giYgCJyCkg/viewform?usp=sf_link"; 
